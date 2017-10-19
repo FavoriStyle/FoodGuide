d4d91538740d4a30fb1939e0ce481989e65619f6
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-20 01:31:24 +0300

    Little update

--- parts/reformat_css.php
+++ parts/reformat_css.php
@@ -1,6 +1,11 @@
 <?php
     (function(){
-        $files = scandir($_SERVER['DOCUMENT_ROOT'] . '/wp-content/cache/autoptimize/css/');
+        $files = [];
+        foreach(scandir($_SERVER['DOCUMENT_ROOT'] . '/wp-content/cache/autoptimize/css/') as $file){
+            if(strlen($file) && preg_match('/^autoptimize_/', $file)){
+                $files[] = $file;
+            }
+        }
         var_dump($files);
     })();
 ?>

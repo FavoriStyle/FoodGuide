c5289646b67da6514d9d77f9e08cf5e4c4ba3def
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-20 16:21:35 +0300

    CSS reformat update
    TODO: strip arguments from fonts urls

--- parts/reformat_css.php
+++ parts/reformat_css.php
@@ -2,10 +2,29 @@
     (function(){
         $files = [];
         foreach(scandir($_SERVER['DOCUMENT_ROOT'] . '/wp-content/cache/autoptimize/css/') as $file){
-            if(strlen($file) && preg_match('/^autoptimize_/', $file)){
+            if(strlen($file) == 48 && preg_match('/^autoptimize_/', $file)){
                 $files[] = $file;
             }
         }
-        //var_dump($files);
+        makeDir(WPMU_PLUGIN_DIR . '/openparts/reformat_css');
+        file_put_contents(WPMU_PLUGIN_DIR . '/openparts/reformat_css/done.list', '', FILE_APPEND | LOCK_EX);
+		$list = json_decode(file_get_contents(WPMU_PLUGIN_DIR . '/openparts/reformat_css/done.list'), true);
+        if (!$list) $list = [];
+        $old_list = $list;
+		foreach ($files as $file){
+			if (!in_array($file, $list)){
+                file_put_contents(
+                    $_SERVER['DOCUMENT_ROOT'] . '/wp-content/cache/autoptimize/css/' . $file,
+                    preg_replace(
+                        '(https?:)?\/\/foodguide.in.ua\/.*?\/fonts?\/(\.\.\/\.\.\/fonts?\/)?',
+                        'https://cdn.jsdelivr.net/gh/FavoriStyle/FoodGuide@0.0.1-a/assets/fonts/',
+                        file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/wp-content/cache/autoptimize/css/' . $file,
+                        true)
+                    ),
+                    LOCK_EX);
+                $list[] = $file;
+            }
+        }
+        if($old_list != $list) file_put_contents(WPMU_PLUGIN_DIR . '/openparts/reformat_css/done.list', json_encode($list), LOCK_EX);
     })();
 ?>

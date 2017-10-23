02a32d064022318d9835896d0bb35a9f72b8d5a4
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-23 12:31:55 +0300

    Added to delete list dragscroll script

--- parts/delete_css_js.php
+++ parts/delete_css_js.php
@@ -10,6 +10,24 @@
             foreach( $wp_styles->queue as $handle ) :
                 echo $handle . ' | ';
             endforeach;
+        });
+        add_action('wp_enqueue_scripts', function(){
+            foreach ([
+                // scripts and styles to remove
+                'scripts' => [
+                    'dragscroll',
+                ],
+                'styles' => [
+                ]
+            ] as $what => $name){
+                if ($what = 'scripts'){
+                    wp_deregister_script($name);
+                    wp_dequeue_script($name);
+                } else {
+                    wp_deregister_style($name);
+                    wp_dequeue_style($name);
+                }
+            }        
         });        
     })();
 ?>

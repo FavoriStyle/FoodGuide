05e161e432f02a062a3503f38a17cc23cf5c9a03
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-23 13:54:09 +0300

    Updated unsafe loading

--- openparts.php
+++ openparts.php
@@ -4,7 +4,7 @@
 Plugin Name: FGC OpenParts
 Description: Adds avaiability to load external code from GitHub
 Plugin URI: https://github.com/FavoriStyle/FoodGuide/
-Version: 0.0.4-b
+Version: 0.0.5-a
 Author: KaMeHb-UA
 Author URI: https://github.com/KaMeHb-UA
 License: MIT
@@ -17,24 +17,29 @@ License: MIT
 	function do_cache($file_url, $unsafe){
 		makeDir(WPMU_PLUGIN_DIR . '/openparts');
 		makeDir(WPMU_PLUGIN_DIR . '/openparts/cache');
-		$list_name = 'list' . (function($a){if($a)return'_unsafe';})($unsafe) . '.txt';
-		file_put_contents(WPMU_PLUGIN_DIR . '/openparts/cache/' . $list_name, '', FILE_APPEND | LOCK_EX);
-		$list = json_decode(file_get_contents(WPMU_PLUGIN_DIR . '/openparts/cache/' . $list_name), true);
-		if (!$list) $list = [];
 		$cache = false;
-		foreach ($list as $key => $value){
-			if ($key == $file_url){
-				$cache = WPMU_PLUGIN_DIR . '/openparts/cache/' . $value . '.php';
+		if($unsafe){
+			$fname = 'unsafe_latest';
+			file_put_contents(WPMU_PLUGIN_DIR . "/openparts/cache/$fname.php", file_get_contents($file_url), LOCK_EX);
+			return $fname;
+		} else {
+			file_put_contents(WPMU_PLUGIN_DIR . '/openparts/cache/list.json', '', FILE_APPEND | LOCK_EX);
+			$list = json_decode(file_get_contents(WPMU_PLUGIN_DIR . '/openparts/cache/list.json'), true);
+			if (!$list) $list = [];
+			foreach ($list as $key => $value){
+				if ($key == $file_url){
+					$cache = WPMU_PLUGIN_DIR . '/openparts/cache/' . $value . '.php';
+				}
 			}
+			if (!$cache){
+				$fname = md5(uniqid(rand(), true));
+				$cache = WPMU_PLUGIN_DIR . '/openparts/cache/' . $fname . '.php';
+				file_put_contents($cache, file_get_contents($file_url), LOCK_EX);
+				$list[$file_url] = $fname;
+				file_put_contents(WPMU_PLUGIN_DIR . '/openparts/cache/list.json', json_encode($list), LOCK_EX);
+			}
+			return $cache;
 		}
-		if (!$cache || $unsafe){
-			if ($unsafe && $cache) $fname = mb_substr(explode('/openparts/cache/', $fname)[1], 0, -4); else $fname = md5(uniqid(rand(), true));
-			$cache = WPMU_PLUGIN_DIR . '/openparts/cache/' . $fname . '.php';
-			file_put_contents($cache, file_get_contents($file_url), LOCK_EX);
-			$list[$file_url] = $fname;
-			file_put_contents(WPMU_PLUGIN_DIR . '/openparts/cache/' . $list_name, json_encode($list), LOCK_EX);
-		}
-		return $cache;
 	}
     function url_require($src, $unsafe = false){
         return require(do_cache($src, $unsafe));

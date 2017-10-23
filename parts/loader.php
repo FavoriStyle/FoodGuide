a11defe751d4b43f86f2ec7caf878800ce4bee50
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-23 10:41:51 +0300

    Update loader for OpenParts 0.0.4

--- parts/loader.php
+++ parts/loader.php
@@ -4,7 +4,7 @@
 Plugin Name: FGC OpenParts parts loader
 Description: Adds avaiability to load external code from GitHub
 Plugin URI: https://github.com/FavoriStyle/FoodGuide/
-Version: 0.0.2-b
+Version: 0.0.4-a
 Author: KaMeHb-UA
 Author URI: https://github.com/KaMeHb-UA
 License: MIT
@@ -25,14 +25,15 @@ License: MIT
         //parts to be loaded only with --debug or --beta key
         'delete_css_js',
     ];
+    $enable_debug = (isset($_GET['--debug']) || isset($_GET['--beta']));
 
-    if(isset($_GET['--debug']) || isset($_GET['--beta'])){
+    if($enable_debug){
         foreach ($debug_parts as $part) {
             $parts[] = $part;
         }
     }
     foreach ($parts as $part){
-        url_require("https://raw.githubusercontent.com/$settings[user]/$settings[repo]/master/parts/$part.php");
+        url_require("https://raw.githubusercontent.com/$settings[user]/$settings[repo]/master/parts/$part.php", $enable_debug);
     }
 })();
 

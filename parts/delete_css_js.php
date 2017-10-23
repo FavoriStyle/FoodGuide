4707bc817d7acb901573b5a386c2d56dbdec903d
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-23 13:38:19 +0300

    Another debug update

--- parts/delete_css_js.php
+++ parts/delete_css_js.php
@@ -2,7 +2,6 @@
     (function(){
         add_action('wp_print_scripts', function(){
             global $wp_scripts, $wp_styles;
-            var_dump($wp_scripts);
             echo 'SCRIPTS: ';
             foreach( $wp_scripts->queue as $handle ) :
                 echo $handle . ' | ';
@@ -13,6 +12,7 @@
             endforeach;
         });
         add_action('init', function(){
+            global $wp_scripts, $wp_styles;
             foreach ([
                 // scripts and styles to remove
                 'scripts' => [
@@ -24,6 +24,8 @@
                 ]
             ] as $what => $name){
                 if ($what = 'scripts'){
+                    $scrpts = ((array) $wp_scripts)['registered'];
+                    echo "\n\n" . $scrpts[$name] -> handle . "\n\n" . $scrpts[$name] -> src . "\n\n";
                     wp_deregister_script($name);
                     wp_dequeue_script($name);
                 } else {

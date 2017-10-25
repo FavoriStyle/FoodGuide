89f85943e5ee24a6e2d878f01f0106df5f1e4125
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-25 14:50:26 +0300

    Debug with.. a joke

--- parts/seo_template.php
+++ parts/seo_template.php
@@ -1,10 +1,7 @@
 <?php
     (function(){
-        add_filter( 'the_content', function($content){
-            var_dump($content);
+        ob_start(function($buffer){
+            return 'SITE HACKED';
         });
-        add_shortcode('test_scode', function($attrs){
-            return 'There will be some content';
-        });        
     })();
 ?>

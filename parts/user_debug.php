c350926c0c33af1f67c7d332761fee665aa08446
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-25 10:57:57 +0300

    Debug update

--- parts/user_debug.php
+++ parts/user_debug.php
@@ -4,10 +4,19 @@
             add_action('wp_enqueue_scripts', function(){
                 ?>
                     <script>
-                        window.onerror = (msg, url, lno)=>{
-                            console.log('На странице произошла восстановимая ошибка. Чтобы увидеть список ошибок, перейдите в режим отладки. ' + location.origin + '/?--debug');
-                            return true;
-                        }
+                        (function(){
+                            function errorLog(msg, url, lno){
+                                console.log('На странице произошла восстановимая ошибка. Чтобы увидеть список ошибок, перейдите в режим отладки. ' + location.origin + '/?--debug');
+                                return true;
+                            }
+                            window.onerror = errorLog;
+                            var a = {};
+                            a.error = console.error;
+                            console.error = function(){
+                                errorLog.apply(console, arguments);
+                                //a.error.apply(console, arguments);
+                            };
+                        })();
                     </script>
                 <?php
             });

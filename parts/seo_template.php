21e0d0523f654d6b199f1d358cd445c4713451fe
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-25 16:22:26 +0300

    Trying to log found seo vars

--- parts/seo_template.php
+++ parts/seo_template.php
@@ -2,7 +2,7 @@
     (function(){
         ob_start();
         
-        add_action('shutdown', function() {
+        add_action('shutdown', function(){
             $final = '';
         
             // We'll need to get the number of ob levels we're in, so that we can iterate over each, collecting
@@ -17,8 +17,12 @@
             echo apply_filters('final_output_seo', $final);
         }, 0);
         
-        add_filter('final_output_seo', function($output) {
-            return 'SITE HACKED';//str_replace('foo', 'bar', $output);
+        add_filter('final_output_seo', function($output){
+            $callback = function($matches){
+                var_dump($matches);
+                return $matches[0];
+            };
+            return preg_replace_callback('/([^\\])\[(categor(ies|y)|name|city|address|N|h1)\]/', $callback, $output);
         });        
     })();
 ?>

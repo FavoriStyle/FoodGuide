ee8ab918500e610d6a26fe126f2441ec6f2a3ef6
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-25 12:20:05 +0300

    Debug update

--- parts/seo_template.php
+++ parts/seo_template.php
@@ -1,5 +1,8 @@
 <?php
     (function(){
+        add_filter( 'the_content', function($content){
+            var_dump($content);
+        });
         add_shortcode('test_scode', function($attrs){
             return 'There will be some content';
         });        

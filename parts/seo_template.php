3985ff3217e23b4e609e036f76e89345f6a9ec70
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-25 10:34:15 +0300

    Updated to shortcode system

--- parts/seo_template.php
+++ parts/seo_template.php
@@ -1,7 +1,7 @@
 <?php
     (function(){
-        add_filter('the_content', function($content){
-            var_dump($content);
+        add_shortcode('test_scode', function($attrs){
+            return 'There will be some content';
         });        
     })();
 ?>

79d16f765d7ca248559558e5b5f79758ca85f9d7
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-26 12:27:45 +0300

    Name definition

--- parts/seo_template.php
+++ parts/seo_template.php
@@ -17,18 +17,23 @@
                 $reg_res = [];
                 if(preg_match('/<h1[^>]*>(.+?)<\\/h1>/mis', $output, $reg_res)) return $reg_res[1]; else return false;
             };
+            $do_case = function($res, $case_mode){
+                if ($case_mode == 2) return mb_strtoupper($res); elseif ($case_mode == 1) return mb_strtoupper($res[0]) . mb_strtolower(mb_substr($res, 1)); else return mb_strtolower($res);
+            };
             $variables = [
                 'category' => function($case_mode /* 0 - first lower; 1 - first upper; 2 - all upper */){
-                    global $post;
-                    ob_start();
-                    var_dump($post);
-                    return ob_get_clean();
+                    //global $post;
+                    //ob_start();
+                    //var_dump($post -> post_title);
+                    //return ob_get_clean();
                     //code
                 },
                 'categories' => function($case_mode){
                     //code
                 },
-                'name' => function($case_mode){
+                'name' => function($case_mode, $do_case){
+                    global $post;
+                    return $do_case($post -> post_title, $case_mode);
                     //code
                 },
                 'city' => function($case_mode){

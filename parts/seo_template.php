e97c1b11481dfd8fda5ad6abc0a54ea073b13c37
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-10-30 18:39:16 +0200

    Завтра всё опишу

--- parts/seo_template.php
+++ parts/seo_template.php
@@ -1,11 +1,24 @@
 <?php
     (function(){
-        $mysql_result = function($sql){
+        $mysql_result = (function($func){
+            return function($sql, $debugConsole = false) use ($func){
+                if (!$debugConsole){
+                    $noop = function(){};
+                    $debugConsole = new class {
+                        public function log($a){}
+                        public function warn($a){}
+                        public function error($a){}
+                    };
+                }
+                return $func($sql, $debugConsole);
+            };
+        })(function($sql, $debugConsole){
             $sql = preg_replace_callback('/FROM\s+`(.+?)`/ms', function($matches){
                 global $wpdb;
                 if(!(mb_strpos($matches[1], $wpdb -> prefix) === 0)) $matches[1] = $wpdb -> prefix . $matches[1];
                 return 'FROM `' . $matches[1] . '`';
             }, $sql);
+            $debugConsole -> log($sql);
             $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
             if (!$mysqli -> connect_errno){
                 $res = [];
@@ -17,7 +30,7 @@
                 }
             }
             return false;
-        };
+        });
         ob_start();
         add_action('shutdown', function(){
             $final = '';
@@ -25,12 +38,18 @@
             for ($i = 0; $i < $levels; $i++){
                 $final .= ob_get_clean();
             }
-            echo apply_filters('place_restricted_css_to_cdn', apply_filters('final_output_seo', $final));
+            echo apply_filters('place_restricted_css_to_cdn', apply_filters('final_output_seo', apply_filters('final_output_seo', $final)));
         }, 0);
         add_filter('final_output_seo', function($output) use ($mysql_result){
             $is = function($class) use ($output){
                 if(preg_match('/<html[^>]*class="[^>"]*( |\\b)' . $class . '[^>"]*"[^>]*>/', $output)) return true; else return false;
             };
+            $is_paginating = false;
+            $page_num = (function() use (&$is_paginating){
+                $reg_res = [];
+                $is_paginating = !!preg_match('/\/page\/(\d+)\//', $_SERVER['REQUEST_URI'], $reg_res);
+                if ($is_paginating) return $reg_res[1]; else return '0';
+            })();
             $is_admin_page = !!preg_match('/\\/wp\\-admin\\//', $_SERVER['REQUEST_URI']);
             $get_first_header = function() use ($output){
                 $reg_res = [];
@@ -54,19 +73,35 @@
                 if ($is_admin_page) return '[{' . $do_case('address', $case_mode) . '}]';
                 if ($ait_post_data) return $ait_post_data['map']['address']; else return '';
             };
+            $get_cat = function($case_mode) use($do_case, $mysql_result){
+                global $post;
+                $a = $mysql_result('SELECT * FROM `posts_main_categories` WHERE `post_id` = ' . $post -> ID);
+                if ($a && count($a) > 0) $a = $a[0]['category_id']; else {
+                    $a = $mysql_result('SELECT `' . get_locale() . '` FROM `custom_translates` WHERE `string` = "default item category id"');
+                    if ($a && count($a) > 0) $a = $a[0][get_locale()]; else $a = false;
+                    if (!$a || $a == '') $a = 0;
+                }
+                $a = $mysql_result('SELECT `name` FROM `terms` WHERE `term_id` = ' . $a);
+                if ($a && count($a) > 0) return $do_case(iconv(mb_detect_encoding($a[0]['name'], mb_detect_order(), true), "UTF-8", $a[0]['name']), $case_mode); else return '';
+            };
             $variables = [
-                'category' => function($case_mode /* 0 - first lower; 1 - first upper; 2 - all upper */) use ($do_case, $is_admin_page, $mysql_result){
+                'category' => function($case_mode /* 0 - first lower; 1 - first upper; 2 - all upper */) use ($is_admin_page, $get_cat){
                     if ($is_admin_page) return '[{' . $do_case('category', $case_mode) . '}]';
-                    global $post;
-                    ob_start();
-                    $a = $mysql_result('SELECT * FROM `posts_main_categories` WHERE `post_id` = ' . $post -> ID);
-                    if ($a && count($a) > 0) $a = $a[0]['category_id']; else {
-                        $a = $mysql_result('SELECT `' . get_locale() . '` FROM `custom_translates` WHERE `string` = "default item category id"');
-                        if ($a && count($a) > 0) $a = $a[0][get_locale()]; else $a = false;
-                        if (!$a || $a == '') $a = 0;
-                    }
-                    $a = $mysql_result('SELECT `name` FROM `terms` WHERE `term_id` = ' . $a);
-                    if ($a && count($a) > 0) return $do_case(iconv(mb_detect_encoding($a[0]['name'], mb_detect_order(), true), "UTF-8", $a[0]['name']), $case_mode); else return '';
+                    return $get_cat($case_mode);
+                },
+                'single_category' => function($case_mode) use ($is_admin_page, $get_cat, $mysql_result){
+                    if ($is_admin_page) return '[{' . $do_case('single_category', $case_mode) . '}]';
+                    $cat = $get_cat($case_mode);
+                    $a = $mysql_result('SELECT `single` FROM `categories_singles` WHERE `category` = FROM_BASE64(\'' . base64_encode($cat) . '\')', (function(){
+                        return new class {
+                            public function log($a){
+                                var_dump($a);
+                            }
+                            public function warn($a){}
+                            public function error($a){}
+                        };
+                    })());
+                    if ($a) return iconv(mb_detect_encoding($a[0]['single'], mb_detect_order(), true), "UTF-8", $a[0]['single']); else return $cat;
                 },
                 'name' => function($case_mode) use ($do_case, $is_admin_page){
                     if ($is_admin_page) return '[{' . $do_case('name', $case_mode) . '}]';
@@ -78,24 +113,30 @@
                     global $post;
                     if ($ait_post_data) return $do_case(explode(',', $ait_post_data['map']['address'])[0], $case_mode); else return '';
                 },
-                'page: _x(lang)' => function($case_mode) use ($do_case, $is_admin_page){
-                    if ($is_admin_page) return '[{' . $do_case('page: _x(lang)', $case_mode) . '}]';
+                'page_x' => function($case_mode) use ($do_case, $is_admin_page){
+                    if ($is_admin_page) return '[{' . $do_case('page_x', $case_mode) . '}]';
                     return $do_case(mb_substr(__('Page %s', 'ait'), 0, -3), $case_mode);
                 },
-                '(save case) => address' => $addr_callback,
-                '(save case) => Address' => $addr_callback,
+                '(save case) => address' => function() use($addr_callback, $do_case){
+                    return $do_case($addr_callback(0), 0);
+                },
+                '(save case) => Address' => function() use($addr_callback, $do_case){
+                    return $do_case($addr_callback(1), 1);
+                },
                 '(save case) => ADDRESS' => function() use($addr_callback, $do_case){
                     return $do_case($addr_callback(2), 2);
                 },
-                '(save case) => N' => function() use ($is, $is_admin_page){
+                '(save case) => N' => function() use ($page_num, $is_admin_page){
                     if ($is_admin_page) return '[{N}]';
-                    $reg_res = [];
-                    if ($is('categories-page') && preg_match('/\/page\/(\d+)\//', $_SERVER['REQUEST_URI'], $reg_res)) return $reg_res[1];
-                    return '1';
+                    return $page_num;
                 },
                 '(save case) => h1' => function() use ($get_first_header, $is_admin_page, $do_case){
                     if ($is_admin_page) return '[{h1}]';
                     return (function($a){if($a)return$a;else return '';})($do_case(trim(strip_tags($get_first_header())), 1));
+                },
+                '(regexp) => setDefault\\(([^\\)]+?)\\);\\s*setNumbered\\(([^\\)]+?)\\)' => function($matches) use ($page_num, $is_admin_page){
+                    if ($is_admin_page) return '[{' . $matches[0] . '}]';
+                    return ($page_num == 0 ? $matches[1] : $matches[2]);
                 },
             ];
             $vars_table = [];
@@ -106,19 +147,38 @@
                 $res = '/([^\\\])\\[\\{(';
                 foreach ($variables as $key => $value){
                     $save_case = false;
+                    $regexp = false;
                     if (strpos($key, '(save case) => ') === 0){
                         $save_case = true;
                         $key = mb_substr($key, 15);
+                    } elseif (strpos($key, '(regexp) => ') === 0){
+                        $regexp = true;
+                        $key = mb_substr($key, 12);
                     }
-                    if (!$save_case) $will = '[' . $perform_regexp_from_str(mb_strtoupper(mb_substr($key, 0, 1)) . mb_strtolower(mb_substr($key, 0, 1))) . ']' . $perform_regexp_from_str(mb_strtolower(mb_substr($key, 1))) . '|' . $perform_regexp_from_str(mb_strtoupper($key)) . '|'; else $will = $key . '|';
+                    if (!$save_case && !$regexp) $will = '[' . $perform_regexp_from_str(mb_strtoupper(mb_substr($key, 0, 1)) . mb_strtolower(mb_substr($key, 0, 1))) . ']' . $perform_regexp_from_str(mb_strtolower(mb_substr($key, 1))) . '|' . $perform_regexp_from_str(mb_strtoupper($key)) . '|'; elseif (!$regexp) $will = $perform_regexp_from_str($key) . '|'; else $will = $key . '|';
                     $res .= $will;
-                    $vars_table['/^' . mb_substr($will, 0, -1) . '$/'] = $value;
+                    $vars_table[($save_case ? '(save case) => ' : '') . ($regexp ? '(regexp) => ' : '') . '/^' . mb_substr($will, 0, -1) . '$/'] = $value;
                 }
                 return mb_substr($res, 0, -1) . ')\\}\\]/';
             })();
             $callback = function($matches) use (&$vars_table){
                 foreach ($vars_table as $key => $value){
-                    if(preg_match($key, $matches[2])) return $matches[1] . $value((function($text){if(mb_strtoupper($text)==$text)return 2;elseif(mb_strtoupper(mb_substr($text, 0, 1)).mb_substr($text,1)==$text)return 1;else return 0;})($matches[2]));
+                    $save_case = false;
+                    $regexp = false;
+                    if (strpos($key, '(save case) => ') === 0){
+                        $save_case = true;
+                        $key = mb_substr($key, 15);
+                    } elseif (strpos($key, '(regexp) => ') === 0){
+                        $regexp = true;
+                        $key = mb_substr($key, 12);
+                    }
+                    if (!$save_case && !$regexp && preg_match($key, $matches[2])) return $matches[1] . $value((function($text){
+                        if (mb_strtoupper($text) == $text) return 2; elseif (mb_strtoupper(mb_substr($text, 0, 1)) . mb_substr($text, 1) == $text) return 1; else return 0;
+                    })($matches[2])); elseif(!$regexp && preg_match($key, $matches[2])) return $matches[1] . $value(); elseif(preg_match($key, $matches[2])) return $matches[1] . $value((function($a){
+                        array_shift($a);
+                        array_shift($a);
+                        return $a;
+                    })($matches));
                 }
             };
             return preg_replace_callback($regexp, $callback, $output);

082abb94884d45a38cf006170b17b0a6e3e2bb72
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-11-02 16:13:54 +0200

    Functional update. Self-controlled $dashicons

--- parts/native_admin_addon.php
+++ parts/native_admin_addon.php
@@ -6,16 +6,18 @@
             if(!in_array($name, $this -> stack)) $this -> stack[] = $name;
             return "dashicons-fa-u-$name";
         }
-        public function generateCSS(){
-            $css = '';
-            foreach($this -> stack as $symbol){
-                $css .= ",.dashicons-fa-u-$symbol:before";
-            }
-            $css = mb_substr($css, 1) . '{font-family:FontAwesome;}';
-            foreach($this -> stack as $symbol){
-                $css .= ".dashicons-fa-u-$symbol:before{content:\"\\$symbol\";}";
-            }
-            return $css;
+        public function __construct(){
+            add_action('admin_enqueue_scripts', function(){
+                $css = '';
+                foreach($this -> stack as $symbol){
+                    $css .= ",.dashicons-fa-u-$symbol:before";
+                }
+                $css = mb_substr($css, 1) . '{font-family:FontAwesome !important}';
+                foreach($this -> stack as $symbol){
+                    $css .= ".dashicons-fa-u-$symbol:before{content:\"\\$symbol\"}";
+                }
+                echo "<style>$css</style>";
+            });
         }
     };
 
@@ -23,10 +25,6 @@
         add_menu_page('Multiple -> Single title', 'Multiple -> Single', 'loco_admin', 'multiple-single-custom-matcher', function(){
             echo 'There will be a page';
         }, $dashicons -> f145);
-    });
-
-    add_action('admin_enqueue_scripts', function() use (&$dashicons){
-        echo '<style>' . $dashicons -> generateCSS() . '</style>';
     });
    
 ?>

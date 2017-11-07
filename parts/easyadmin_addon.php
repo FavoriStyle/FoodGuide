66a0d792b1b41f0083d34e2af37b45c293266c0c
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-11-07 21:59:48 +0200

    Renewed hook system, support for custom icons, colors and other classes

--- parts/easyadmin_addon.php
+++ parts/easyadmin_addon.php
@@ -4,12 +4,14 @@
         private $stack = [];
         private $colors_stack = [];
         private $colors = [
-            'blue' => '#0035ff',
+            'red' => '#ff0000',
+            'green' => '#00ff00',
+            'blue' => '#0000ff',
         ];
         public function __get($name){
             if(!in_array($name, $this -> stack) && !isset($this -> colors[$name])) $this -> stack[] = $name; elseif(isset($this -> colors[$name])){
                 if (!in_array($name, $this -> colors_stack)) $this -> colors_stack[] = $name;
-                return " dashicons-fa-color-$name";
+                return "dashicons-fa-color-$name";
             }
             return "dashicons-fa-u-$name";
         }
@@ -17,14 +19,14 @@
             add_action('admin_enqueue_scripts', function(){
                 $css = '';
                 foreach($this -> stack as $symbol){
-                    $css .= ",.dashicons-fa-u-$symbol:before";
+                    $css .= ",.dashicons-fa-special-holder.dashicons-fa-u-$symbol:before";
                 }
                 $css = mb_substr($css, 1) . '{font-family:FontAwesome !important}';
                 foreach($this -> stack as $symbol){
-                    $css .= ".dashicons-fa-u-$symbol:before{content:\"\\$symbol\"}";
+                    $css .= ".dashicons-fa-special-holder.dashicons-fa-u-$symbol:before{content:\"\\$symbol\"}";
                 }
                 foreach($this -> colors_stack as $color){
-                    $css .= ".dashicons-fa-color-$color:before{color:" . $this -> colors[$color] . " !important}";
+                    $css .= ".dashicons-fa-special-holder.dashicons-fa-color-$color:before{color:" . $this -> colors[$color] . " !important}";
                 }
                 echo "<style>$css</style>";
             });
@@ -33,69 +35,73 @@
 
     $menu = new class{
         private $menu = [];
-        public function addMenu($header, $href, $icon, $parent = -1){
+        public function addMenu($header, $href, $callback, $classes, $parent = -1){
             if($parent + 1){
                 array_push($this -> menu[$parent]['childs'], [
                     'header' => $header,
-                    'href' => $href
+                    'href' => $href,
+                    'callback' => $callback
                 ]);
             } else {
                 return array_push($this -> menu, [
                     'header' => $header,
                     'href' => $href,
-                    'icon' => $icon,
-                    'iscurrent' => (function($a) use ($href){
-                        return !!preg_match('/' . str_replace('/', '\\/', preg_quote($href)) . '([&\\?].*)?$/', $a);
-                    })($_SERVER['REQUEST_URI']),
-                    'childs' => []
+                    'icon' => 'dashicons-fa-special-holder',
+                    'classes' => $classes,
+                    'childs' => [],
+                    'callback' => $callback
                 ]) - 1;
             }
         }
-        public function generate_menu(){
-            $str = '';
-            $class_list = [
-                'submenu' => [
-                    'wp-has-submenu',
-                ],
-                'wo_submenu' => [],
-                'active' => [
-                    'wp-has-current-submenu',
-                ],
-                'non-active' => [
-                    'wp-not-current-submenu',
-                ],
-                'defaults' => []
-            ];
-            foreach ($this -> menu as $element){
-                $element['childs_count'] = count($element['childs']);
-                $str .= '<li class="' . (function($current) use ($class_list){
-                    if ($current) return implode(' ', $class_list['active']); else return implode(' ', $class_list['non-active']);
-                })($element['iscurrent']) .' wp-has-submenu" id="toplevel_page_items_page_new">
-                            <a href="' . $element['href'] . '" aria-haspopup="false">
-                                <div class="wp-menu-arrow"><div></div></div>
-                                <div class="wp-menu-image dashicons-before ' . $element['icon'] . '"><br></div>
-                                <div class="wp-menu-name">' . $element['header'] . '</div></a>' . (function() use ($element){
-                                    $str = '';
-                                    if($element['childs_count']){
-                                        $str = '<ul class="wp-submenu wp-submenu-wrap"><li class="wp-submenu-head" aria-hidden="true">' . $element['header'] . '</li>';
-                                        foreach ($element['childs'] as $i => $child){
-                                            $first = (!$i ? ' class="wp-first-item"' : '');
-                                            $str .= '<li' . $first . '><a href="' . $child['href'] . '"' . $first . '>' . $child['header'] . '</a></li>';
-                                        }
-                                        $str .= '</ul>';
-                                    }
-                                    return $str;
-                                })($element['childs'], $element['iscurrent']);
-                $str .= '</li>';
-            }
-            return $str;
+        public function __construct(){
+            $stack = [];
+            add_action('admin_menu', function() use (&$stack){
+                foreach ($this -> menu as $element){
+                    $stack[] = [$element['header'], $element['classes']];
+                    add_menu_page($element['header'], $element['header'], /* capability */ 'read', $element['href'], function(){echo $element['callback']();}, $element['icon']);
+                    foreach($element['childs'] as $child){
+                        add_submenu_page($element['href'], $child['header'], $child['header'], 'read', $child['href'], function(){echo $child['callback']();});
+                    }
+                }
+            });
+            add_action('admin_enqueue_scripts', function() use (&$stack){
+                ?>
+                <script>
+                    document.addEventListener('DOMContentLoaded', function(){
+                        var a = document.getElementsByTagName('body')[0].classList, b = <?php echo json_encode($stack); ?>;
+                        for(var i = 0; i < a.length; i++){
+                            if (a[i] == 'ait-easy-admin-enabled'){
+                                let i, a = document.querySelectorAll('#easyadmin-main-menu > li > a > .dashicons-fa-special-holder');
+                                for(i = 0; i < a.length; i++){
+                                    b.forEach(function(e){
+                                        if(a[i].parentNode.lastChild.innerHTML == e[0]) e[1].forEach(function(cls){a[i].classList.add(cls)});
+                                    });
+                                }
+                            }
+                        }
+                    });
+                </script>
+                <?php
+                /*
+                foreach($stack as $el){
+                    if(is_single() && $item -> title == $el[0]){
+                        $classes[] = implode(' ', $el[1]);
+                    }
+                }
+                return $classes;
+                */
+            } , 10 , 2);
         }
     };
-    add_filter('easyadmin_addon', function($output) use (&$menu){
-        return preg_replace('/<li[^>]*\\sid="collapse-menu"[^>]*>[\\s\\S]*?<\\/li>/', $menu -> generate_menu() . '$0', $output);
-    });
-    $parent = $menu -> addMenu('Items new', '/wp-admin/profile.php', implode(' ', [$FontAwesome -> f00b, $FontAwesome -> blue]));
-    $menu -> addMenu('Сабзиро', '#этовсёчтоунегоможетбыть', null, $parent);
-    $menu -> addMenu('Сабзиро2', '#этовсёчтоунегоможетбыть2', null, $parent);
+
+    $parent = $menu -> addMenu('Items new', 'new_page.ea_addon', function(){
+        return 'Тупо кастомная страница';
+    }, [$FontAwesome -> f00b, $FontAwesome -> blue]);
+    $menu -> addMenu('List', 'new_page2.ea_addon', function(){
+        return 'Тупо кастомная страница';
+    }, null, $parent);
+    $menu -> addMenu('Add new', 'new_page3.ea_addon', function(){
+        return 'Тупо кастомная страница';
+    }, null, $parent);
    
 ?>

7046a0da43979d7e2493c48559b4b49112dad702
Влад KaMeHb Марченко <blade.whiteblack@gmail.com>
2017-11-11 21:36:41 +0200

    Using a static global mysql_result function

--- parts/native_admin_addon.php
+++ parts/native_admin_addon.php
@@ -1,41 +1,5 @@
 <?php
 
-    $mysql_result = (function($func){
-        return function($sql, $debugConsole = false) use ($func){
-            if (!$debugConsole){
-                $debugConsole = new class {
-                    public function log($a){}
-                    public function warn($a){}
-                    public function error($a){}
-                };
-            }
-            return $func($sql, $debugConsole);
-        };
-    })(function($sql, $debugConsole){
-        $sql = preg_replace_callback('/FROM\s+`(.+?)`/ms', function($matches){
-            global $wpdb;
-            if(!(mb_strpos($matches[1], $wpdb -> prefix) === 0)) $matches[1] = $wpdb -> prefix . $matches[1];
-            return 'FROM `' . $matches[1] . '`';
-        }, preg_replace_callback('/JOIN\s+`(.+?)`/ms', function($matches){
-            global $wpdb;
-            if(!(mb_strpos($matches[1], $wpdb -> prefix) === 0)) $matches[1] = $wpdb -> prefix . $matches[1];
-            return 'JOIN `' . $matches[1] . '`';
-        }, $sql));
-        $debugConsole -> log($sql);
-        $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-        if (!$mysqli -> connect_errno){
-            $res = [];
-            $result = $mysqli -> query($sql);
-            if($result){
-                while($res[] = $result -> fetch_assoc()){/*like a null loop*/}
-                array_pop($res);
-                $debugConsole -> log($res);
-                return $res;
-            } else $debugConsole -> error('Cannon get result: ' . $result);
-        } else $debugConsole -> error('DB Connection error ' . $mysqli -> connect_errno);
-        return false;
-    });
-
     $utf8 = function($str){
         return iconv(mb_detect_encoding($str, mb_detect_order(), true), "UTF-8", $str);
     };
@@ -81,12 +45,12 @@
         }
     };
 
-    add_action('admin_menu', function() use (&$FontAwesome, &$templates, $mysql_result, $utf8){
+    add_action('admin_menu', function() use (&$FontAwesome, &$templates, $utf8){
         /*must be initialized for pseudoasync callback first*/ $FontAwesome -> f0c7;
-        add_menu_page('Multiple -> Single title', 'Multiple -> Single', 'loco_admin', 'multiple-single-custom-matcher', function() use (&$FontAwesome, &$templates, $mysql_result, $utf8){
+        add_menu_page('Multiple -> Single title', 'Multiple -> Single', 'loco_admin', 'multiple-single-custom-matcher', function() use (&$FontAwesome, &$templates, $utf8){
             $templates -> multiple_to_single_matching -> set('heading', __('Multiple and single categories names matching', 'ait-admin'));
             $templates -> multiple_to_single_matching -> set('mtsm_tip', 'Tip will be here');
-            $temp = $mysql_result('SELECT * FROM `categories_singles`', new class{
+            $temp = staticGlobals::mysql_result('SELECT * FROM `categories_singles`', new class{
                 public function log($a){
                     //var_dump($a);
                 }
@@ -101,7 +65,7 @@
                 $temp[$utf8($row['category'])] = $utf8($row['single']);
                 unset($temp[$i]);
             }
-            foreach($mysql_result('SELECT terms.name FROM `terms` as terms JOIN `term_taxonomy` as tax WHERE tax.taxonomy = \'ait-items\' AND terms.term_id = tax.term_id AND tax.parent = 0') as $row){
+            foreach(staticGlobals::mysql_result('SELECT terms.name FROM `terms` as terms JOIN `term_taxonomy` as tax WHERE tax.taxonomy = \'ait-items\' AND terms.term_id = tax.term_id AND tax.parent = 0') as $row){
                 $row['name'] = $utf8($row['name']);
                 $row['name'] = mb_strtoupper(mb_substr($row['name'], 0, 1)) . mb_strtolower(mb_substr($row['name'], 1));
                 if(!isset($temp[$row['name']])) $temp[$row['name']] = '';

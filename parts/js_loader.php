eb80d73f19365e6bb71cd02b18527713256dbfb4
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-12-07 21:44:29 +0200

    First jsloader fix

--- parts/js_loader.php
+++ parts/js_loader.php
@@ -56,85 +56,87 @@ License: MIT
         $part['src'] = "https://cdn.jsdelivr.net/gh/$settings[user]/$settings[repo]@$latest_release/assets/js/$part[src].min.js";
         $parts[$i] = $part;
     }
-    ?>
-    <script>
-        (function(){
-            if (!window['app']) app = {};
-            app.waitForBody = function(func, args = []){
-                setTimeout(function(){
-                    if(document.body) func.apply(document, args); else app.waitForBody(func, args);
-                }, 100);
-            };
-            app.appendToBody = function(DOMElement){
-                try{
-                    document.body.appendChild(DOMElement);
-                } catch(e) {
-                    app.waitForBody(function(){
+    add_action('wp_enqueue_scripts', function() use ($parts){
+        ?>
+        <script>
+            (function(){
+                if (!window['app']) app = {};
+                app.waitForBody = function(func, args = []){
+                    setTimeout(function(){
+                        if(document.body) func.apply(document, args); else app.waitForBody(func, args);
+                    }, 100);
+                };
+                app.appendToBody = function(DOMElement){
+                    try{
                         document.body.appendChild(DOMElement);
-                    })
-                }
-            };
-            app.extend = function(oldObj, newObj){
-                for(var i in newObj){
-                    oldObj[i] = newObj[i];
-                }
-                return oldObj;
-            };
-            app.createElement = function(settings){
-                settings = app.extend({
-                    name : 'div',
-                    html : '',
-                    attribs : {}
-                }, settings);
-                var el = document.createElement(settings.name);
-                el.innerHTML = settings.html;
-                for (var i in settings.attribs){
-                    el.setAttribute(i, settings.attribs[i]);
-                }
-                return el;
-            };
-            function appendAsyncScript(code, src = false, dependsOn = []){
-                let dependsLoaded = true;
-                dependsOn.forEach(globalObj => {
-                    if (window[globalObj] == undefined) dependsLoaded = false;
-                });
-                if (dependsLoaded){
-                    var el = {
-                        name: 'script',
-                        html : code.toString(),
-                        attribs: {
-                            type : 'text/javascript',
-                            async : '',
-                            defer : '',
-                        }
-                    };
-                    if(src) el.attribs.src = src;
-                    app.appendToBody(app.createElement(el));
-                } else setTimeout(() => {
-                    appendAsyncScript(code, src, dependsOn);
-                }, 100);
-            };
-            <?php echo json_encode($parts); ?>.forEach(function(e){
-                <?php
-                    if (_USER_DEBUG_MODE){
-                ?>
-                        if(/^https:\/\/raw.githubusercontent.com\//.test(e)){
-                            var xhr = new XMLHttpRequest();
-                            xhr.open('GET', e, true);
-                            xhr.send();
-                            xhr.onreadystatechange = function(){
-                                if (xhr.readyState != 4) return;
-                                if (xhr.status == 200){
-                                    appendAsyncScript(xhr.responseText, false, e.deps);
-                                }
-                            }
-                        } else <?php
+                    } catch(e) {
+                        app.waitForBody(function(){
+                            document.body.appendChild(DOMElement);
+                        })
                     }
-                ?> appendAsyncScript('', e.src, e.deps);
-            });
-        })();
-    </script>
-    <?
+                };
+                app.extend = function(oldObj, newObj){
+                    for(var i in newObj){
+                        oldObj[i] = newObj[i];
+                    }
+                    return oldObj;
+                };
+                app.createElement = function(settings){
+                    settings = app.extend({
+                        name : 'div',
+                        html : '',
+                        attribs : {}
+                    }, settings);
+                    var el = document.createElement(settings.name);
+                    el.innerHTML = settings.html;
+                    for (var i in settings.attribs){
+                        el.setAttribute(i, settings.attribs[i]);
+                    }
+                    return el;
+                };
+                function appendAsyncScript(code, src = false, dependsOn = []){
+                    let dependsLoaded = true;
+                    dependsOn.forEach(globalObj => {
+                        if (window[globalObj] == undefined) dependsLoaded = false;
+                    });
+                    if (dependsLoaded){
+                        var el = {
+                            name: 'script',
+                            html : code.toString(),
+                            attribs: {
+                                type : 'text/javascript',
+                                async : '',
+                                defer : '',
+                            }
+                        };
+                        if(src) el.attribs.src = src;
+                        app.appendToBody(app.createElement(el));
+                    } else setTimeout(() => {
+                        appendAsyncScript(code, src, dependsOn);
+                    }, 100);
+                };
+                <?php echo json_encode($parts); ?>.forEach(function(e){
+                    <?php
+                        if (_USER_DEBUG_MODE){
+                    ?>
+                            if(/^https:\/\/raw.githubusercontent.com\//.test(e)){
+                                var xhr = new XMLHttpRequest();
+                                xhr.open('GET', e, true);
+                                xhr.send();
+                                xhr.onreadystatechange = function(){
+                                    if (xhr.readyState != 4) return;
+                                    if (xhr.status == 200){
+                                        appendAsyncScript(xhr.responseText, false, e.deps);
+                                    }
+                                }
+                            } else <?php
+                        }
+                    ?> appendAsyncScript('', e.src, e.deps);
+                });
+            })();
+        </script>
+        <?
+    });
 })();
 
 ?>

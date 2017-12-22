213ce131ab07e23869fe50a1252c5b1f408989e3
KaMeHb-UA <blade.whiteblack@gmail.com>
2017-12-22 16:49:49 +0200

    Changed alt text for image on WtBH widget

--- assets/js/main.js
+++ assets/js/main.js
@@ -430,7 +430,7 @@ function stack_prepare(){
         if(is('main-page')){
             var col1 = $('#main > div.main-sections > section.want-to-be-here').find('.elm-item-organizer-container > .item.item-last');
             col1.find('div.item-thumbnail > a > div').remove();
-            col1.find('div.item-thumbnail > a > img').attr('src','/wp-content/themes/FGC/design/img/want-to-be-here.jpg');
+            col1.find('div.item-thumbnail > a > img').attr('src','/wp-content/themes/FGC/design/img/want-to-be-here.jpg').attr('alt', dictionary.translate('want to be here'));
             col1.find('div.item-header > .item-title > a > *').html(dictionary.translate('want to be here'));
             col1.find('div.item-footer').css('display','none');
             subscribe(col1.find('a').removeAttr('href'));

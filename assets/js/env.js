53a26384fcf02663c6307824a73fdbe46d3f4253
KaMeHb-UA <marlock@etlgr.com>
2018-05-11 19:26:05 +0300

    Shortcut for creating elements

--- assets/js/env.js
+++ assets/js/env.js
@@ -155,4 +155,10 @@ module.exports = {
             return true
         }
     }),
+    _: ({name, attrs, html}) => {
+        var elem = document.createElement(name || 'div');
+        for(var i in (attrs || {})) elem.setAttribute(i, attrs[i]);
+        elem.innerHTML = html || '';
+        return elem;
+    }
 }

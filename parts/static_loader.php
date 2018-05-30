2424ed98292f49e76ebfe4118f74d4caa6825b4a
KaMeHb-UA <marlock@etlgr.com>
2018-05-30 21:28:47 +0300

    Next revision

--- parts/static_loader.php
+++ parts/static_loader.php
@@ -10,7 +10,7 @@ Author URI: https://github.com/KaMeHb-UA
 License: MIT
 */
 
-$target_version = '6a97801d9c8cfbf1704122cceadf60c96bff1a98';
+$target_version = 'c5e8041d5e8c76663dd1cf9605e51d1c3ab61f1e';
 (function() use ($target_version){
     add_action('wp_enqueue_scripts', function() use ($target_version){ ?>
         <script src="https://foodguide.in.ua/wp-includes/js/jquery/jquery.js"></script>

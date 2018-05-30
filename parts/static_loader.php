62680cc5764cb9bf2d7278401055bd2b5f30a88b
KaMeHb-UA <marlock@etlgr.com>
2018-05-30 16:22:57 +0300

    New target commit

--- parts/static_loader.php
+++ parts/static_loader.php
@@ -10,7 +10,7 @@ Author URI: https://github.com/KaMeHb-UA
 License: MIT
 */
 
-$target_version = '1c1615b8ec3616b265233f9e3d0474f2a0382ab6';
+$target_version = 'ec1291957da565ff297b9415f952dd3bbe112556';
 (function() use ($target_version){
     add_action('wp_enqueue_scripts', function() use ($target_version){ ?>
         <script src="https://cdn.jsdelivr.net/gh/FavoriStyle/FoodGuide@<?php echo $target_version; ?>/assets/js/main.js" async defer></script>

db05de0cf312ea20a03a7be3e66fb50d2bfcef20
KaMeHb-UA <marlock@etlgr.com>
2019-06-10 15:38:08 +0300

    pages renamed to parts for better semantics. Added ability to load index asynchronously

--- assets/js/next/index.js
+++ assets/js/next/index.js
@@ -1,8 +1,13 @@
-const pages = {
-    singleItem: document.body.classList.contains('single-ait-item'),
+const parts = {
+    allboomsCommentsWidget: () => document.body.classList.contains('single-ait-item'),
 };
 
-// loads only needed modules. Every module must have side effect
-for(var page in pages){
-    if(pages[page]) import(`./pages/${page}.js`)
+function loader(){
+    // loads only needed modules. Every module must have side effect
+    // spawns only after dom is ready
+    for(var part in parts){
+        if(parts[part]()) import(`./parts/${part}.js`)
+    }
 }
+
+if (document.readyState !== "loading") loader(); else document.addEventListener('DOMContentLoaded', loader);
